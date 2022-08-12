194f2a964b0db14871b2dd32b5719d8c000c6972
Mahalakshmi08 <thevar.mahalakshmi18@siesgst.ac.in>
2022-08-12 14:30:50 +0530

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyCalc;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Calc calc = new Calc();
+            Console.WriteLine($"Sum = {calc.Add(70,80)}");
+            Console.WriteLine($"Multiply = {calc.Add(4, 5)}");
+            Console.ReadLine();
+        }
+    }
+}
